e3759a2afeb19d095e9d2074fb367a0061428f71
Mateusz Piasecki <piaszuu@gmail.com>
2023-01-24 18:30:19 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,66 +16,69 @@ export const App = () => {
   // Funkcja pobierająca obrazy z API
   const fetchImages = async () => {
     setIsLoading(true);
+
+    // Zerowanie stanu images tylko przy wyszukiwaniu nowych zdjęć i ustawieniu strony na 1. W przeciwnym razie dodaje nowe obrazy do listy.
     if (page === 1) {
-      // Zerowanie stanu images tylko przy wyszukiwaniu nowych zdjęć
       setImages([]);
+    } else {
+      setImages([...images]);
     }
+
     const response = await fetch(
       `https://pixabay.com/api/?key=${'32295934-1722a349e231221c1d2235015'}&q=${searchTerm}&page=${page}`
     );
+
     const data = await response.json();
-    setImages([...images, ...data.hits]); // Dodawanie nowych zdjęć do listy
+
+    // Dodawanie nowych zdjęć do listy lub ustawienie pustej tablicy jeśli wyszukiwanie jest wykonywane po raz pierwszy.
+
+    if (page === 1) {
+      setImages([...data.hits]);
+    } else {
+      setImages([...images, ...data.hits]);
+    }
+
     setIsLoading(false);
   };
 
-  // Obsługa formularza wyszukiwania
+  // Obsługa formularza wyszukiwania - zresetuj images na pustą tablicę i uruchom funkcję fetchImages() aby pobrać nowe obrazy.
+
   const handleSearch = event => {
     event.preventDefault();
-    setImages([]); // Ustaw początkową wartość stanu `images` na pustą tablicę
-    setPage(1);
+    setImages([]);
     fetchImages();
   };
 
-  // Obsługa zmiany wartości w polu wyszukiwania
+  // Obsługa zmiany wartości w polu wyszukiwania - ustaw searchTerm na wartość pola inputu.
+
   const handleSearchChange = event => {
     setSearchTerm(event.target.value);
   };
 
-  // Obsługa przycisku "Pokaż więcej"
+  // Obsługa przycisku "Pokaż więcej" - inkrementuj page o 1 aby pobrać kolejną stronę obrazów z API Pixabay.
+
   const handleLoadMore = () => {
     setPage(page + 1);
   };
 
-  // Obsługa otwarcia modala
+  // Obsługa otwarcia modala - ustaw selectedImage i isModalOpen na odpowiednie wartości aby otworzyć modal z odpowiednim obrazem.
+
   const handleModalOpen = image => {
     setSelectedImage(image);
     setIsModalOpen(true);
   };
 
-  // Obsługa zamknięcia modala
+  // Obsługa zamknięcia modala - ustaw isModalOpen na false aby zamknąć modal
+
   const handleModalClose = () => {
     setIsModalOpen(false);
   };
 
   useEffect(() => {
     if (searchTerm) {
-      const fetchMoreImages = async () => {
-        setIsLoading(true);
-        if (page === 1) {
-          // Zerowanie stanu images tylko przy wyszukiwaniu nowych zdjęć
-          setImages([]);
-        }
-        const response = await fetch(
-          `https://pixabay.com/api/?key=${'32295934-1722a349e231221c1d2235015'}&q=${searchTerm}&page=${page}`
-        );
-        const data = await response.json();
-        setImages([...images, ...data.hits]); // Dodawanie nowych zdjęć do listy
-        setIsLoading(false);
-      };
-      fetchMoreImages();
+      fetchImages();
     }
   }, [page]);
-
   return (
     <div
       style={{
@@ -85,19 +88,20 @@ export const App = () => {
         paddingBottom: '24px',
       }}
     >
+      {' '}
       <Searchbar
         searchTerm={searchTerm}
         onChange={handleSearchChange}
         onSubmit={handleSearch}
-      />
+      />{' '}
       {isModalOpen && (
         <Modal image={selectedImage} onModalClose={handleModalClose} />
-      )}
-      {isLoading && <Loader />}
+      )}{' '}
+      {isLoading && <Loader />}{' '}
       {images.length > 0 && !isLoading && (
         <ImageGallery images={images} onModalOpen={handleModalOpen} />
-      )}
-      {images.length > 0 && !isLoading && <Button onClick={handleLoadMore} />}
+      )}{' '}
+      {images.length > 0 && !isLoading && <Button onClick={handleLoadMore} />}{' '}
     </div>
   );
 };
